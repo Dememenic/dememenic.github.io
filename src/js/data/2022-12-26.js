d3b321efe3dd31d61f86cf6969f75db7d95fa764
Dememenic <50307836+Dememenic@users.noreply.github.com>
2022-12-26 17:01:41 -0500

    lol

---
+++ src/js/data/2022-12-26.js
@@ -0,0 +1,462 @@
+dataSetVersion = "2022-12-26"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+
+  {
+    name: "Don't check this lmfao",
+    key: "twostar",
+    tooltip: "why are you hovering over this",
+    checked: false
+  },
+
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "AA72",
+    img: "Scs4bgp.jpg",
+  },
+  {
+    name: "Aisha",
+    img: "OOyfN08.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Akayuki",
+    img: "0ROFZxc.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Alef",
+    img: "MJbw9p8.jpg",
+  },
+  {
+    name: "Amy",
+    img: "dlDoxWW.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Mk.99",
+    img: "n7rT5fT.jpg",
+  },
+  {
+    name: "Aoba",
+    img: "sUFeOFd.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Ara",
+    img: "J4BedxB.jpg",
+  },
+  {
+    name: "Arabelle",
+    img: "y2M7Z1R.jpg",
+  },
+  {
+    name: "Bari",
+    img: "yadEo23.jpg",
+  },
+  {
+    name: "Beach Sohee",
+    img: "jKEpv3R.jpg",
+  },
+  {
+    name: "Lifeguard Yuze",
+    img: "GspUlzs.jpg",
+  },
+  {
+    name: "Beth",
+    img: "cV4QLiz.jpg",
+  },
+  {
+    name: "Bianca",
+    img: "0jFPKmd.jpg",
+  },
+  {
+    name: "Carol",
+    img: "OgIxubE.jpg",
+  },
+  {
+    name: "Catherine",
+    img: "Pvwhyke.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Clara",
+    img: "2DrnCeC.jpg",
+  },
+  {
+    name: "Claude",
+    img: "DosnKrL.jpg",
+  },
+  {
+    name: "Coco",
+    img: "p9pq33A.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Craig",
+    img: "UhcJnaQ.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Dolf",
+    img: "NNFl6zF.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Eleanor",
+    img: "MY4QYIX.jpg",
+  },
+  {
+    name: "Elvira",
+    img: "vB8ZS60.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Erina",
+    img: "0Riru3a.jpg",
+  },
+  {
+    name: "Eugene",
+    img: "5ysd5XI.jpg",
+  },
+  {
+    name: "Eva",
+    img: "HpjTuea.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Favi",
+    img: "G5SyRNs.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Female Knight",
+    img: "SilUUsf.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Future Princess",
+    img: "1k4YWVI.jpg",
+  },
+  {
+    name: "Future Knight",
+    img: "DU5J222.jpg",
+  },
+  {
+    name: "Gabriel",
+    img: "F0WqDAc.jpg",
+  },
+  {
+    name: "Garam",
+    img: "9qsFce7.jpg",
+  },
+  {
+    name: "Girgas",
+    img: "wHxl3Ux.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Gourry",
+    img: "HubvDM2.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Gremory",
+    img: "nLnwC8m.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Hana",
+    img: "1XMUJqo.jpg",
+  },
+  {
+    name: "Hekate",
+    img: "FByqKz6.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Idol Eva",
+    img: "fBLiwU5.jpg",
+  },
+  {
+    name: "Kamael",
+    img: "ppFhDmj.jpg",
+  },
+  {
+    name: "Kanna",
+    img: "wHY3MAE.jpg",
+  },
+  {
+    name: "Karina",
+    img: "gCO9aT7.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Lahn",
+    img: "Bz7JUTi.jpg",
+  },
+  {
+    name: "Lapice",
+    img: "spkAsYi.jpg",
+  },
+  {
+    name: "Lavi",
+    img: "lFP3JV1.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Lilith",
+    img: "cwVo2r8.jpg",
+  },
+  {
+    name: "Lina",
+    img: "RMikd4X.jpg",
+  },
+  {
+    name: "Loraine",
+    img: "rZICOYf.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Lucy",
+    img: "VZNcuB2.jpg",
+  },
+  {
+    name: "Lupina",
+    img: "ORTiA0R.jpg",
+  },
+  {
+    name: "Lynn",
+    img: "MkUE1B2.jpg",
+  },
+  {
+    name: "Mad Panda Trio",
+    img: "Gk2a98z.jpg",
+  },
+  {
+    name: "Marianne",
+    img: "MC6uO5J.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Marina",
+    img: "eBRnwsp.jpg",
+  },
+  {
+    name: "Marvin",
+    img: "lPS9h9v.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Mayreel",
+    img: "DF9ztSg.jpg",
+  },
+  {
+    name: "Mei",
+    img: "N0wNlVl.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Miss Chrom",
+    img: "GnF0DvL.jpg",
+  },
+  {
+    name: "Miya",
+    img: "8kjYKwi.jpg",
+  },
+  {
+    name: "Mk.2",
+    img: "iEgEyPq.jpg",
+  },
+  {
+    name: "Male Knight",
+    img: "unkcYnw.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Nari",
+    img: "kZLWK6g.jpg",
+  },
+  {
+    name: "Neva",
+    img: "w43Lsfg.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Noxia",
+    img: "sGeSinT.jpg",
+  },
+  {
+
+    name: "Oghma",
+    img: "EbMDToR.jpg",
+  },
+  {
+    name: "Orca",
+    img: "kccsC78.jpg",
+  },
+  {
+    name: "Parvati",
+    img: "TswcYNf.jpg",
+  },
+  {
+    name: "Piltvice",
+    img: "y9cOmxp.jpg",
+  },
+  {
+    name: "Priscilla",
+    img: "zciYDcq.jpg",
+  },
+  {
+    name: "Rachel",
+    img: "dpG50xO.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Ranpang",
+    img: "TGRfsqP.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Rey",
+    img: "biZn3GW.jpg",
+  },
+  {
+    name: "Rie",
+    img: "SI3Ceh1.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Rue",
+    img: "uIx028O.jpg",
+  },
+  {
+    name: "Scintilla",
+    img: "FivbsNW.jpg",
+  },
+  {
+    name: "Shapira",
+    img: "Nal4SQY.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Summer Loraine",
+    img: "RqET94Y.jpg",
+  },
+  {
+    name: "Sohee",
+    img: "s2FjgB9.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  { 
+    name: "Tinia",
+    img: "7kxqRdm.jpg",
+  },
+  {
+    name: "Veronica",
+    img: "z5XJGSI.jpg",
+  },
+  {
+    name: "Vishuvac",
+    img: "8J3pvou.jpg",
+  },
+  {
+    name: "Xellos",
+    img: "V9fh90n.jpg",
+  },
+  {
+    name: "Yuze",
+    img: "gmu2iYL.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Andras",
+    img: "NHCLvtK.jpg",
+  },
+  {
+    name: "Crosselle",
+    img: "4SdOaoL.jpg",
+  },
+  {
+    name: "Fei",
+    img: "8Fu9Ba3.jpg",
+    opts: {
+      twostar: true
+    }
+  },
+  {
+    name: "Pymon",
+    img: "2SSrccG.jpg",
+  },
+  {
+    name: "Sumire",
+    img: "ywYybLA.jpg",
+  },
+  {
+    name: "Valencia",
+    img: "B0jFrGP.jpg",
+  },
+];
